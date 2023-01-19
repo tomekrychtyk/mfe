52d6fe9b875898218f68971840c4f85453cf9ea7
Tomek Rychtyk <tomek@rychtyk.pl>
2023-01-19 07:26:28 +0100

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return (
+    <React.Fragment>
+      <h1>Hi there, marketing</h1>
+      <MarketingApp />
+    </React.Fragment>
+  );
+};
