69f99a27c17e47d3840f2152a92f86edbaefc9a4
Tomek Rychtyk <tomek@rychtyk.pl>
2023-01-23 11:16:37 +0100

    Testing marketing deployment

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,7 +4,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-// test 1
+// test 2
 export default () => {
   return (
     <div>
