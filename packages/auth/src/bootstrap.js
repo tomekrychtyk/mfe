0f43100e371a19be93e9aa336b88d4c7195b5d2b
Tomek Rychtyk <tomek@rychtyk.pl>
2023-02-02 11:45:11 +0100

    Auth app scaffolding

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (nextPathname !== pathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('_auth-dev-root');
+
+  if (el) {
+    mount(el, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
